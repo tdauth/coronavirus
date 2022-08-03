c1a398be85b2986806fbee37ed20c9db87e7f269
Barade <barade.barade@web.de>
2022-08-03 15:52:29 +0200

    1.1

--- updatemap.bat
+++ updatemap.bat
@@ -1,2 +1,2 @@
-copy C:\Users\Tamino\Documents\Projekte\coronavirus\coronavirus1.0.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+copy C:\Users\Tamino\Documents\Projekte\coronavirus\coronavirus1.1.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 pause
